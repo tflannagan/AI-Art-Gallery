dc45e4d74cc1f055386cb0d0c3ee1e93cc51ffc5
Ty Flannagan <tylerflannagan@yahoo.com>
2022-11-09 15:27:38 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 align="center">AI Art Gallery</h1>
+     <wrapper>
+      <section className="App">
+        <img  alt="img" src={require("./assets/img-1.png")} />
+        <img alt="img" src={require("./assets/img-3.png")} />
+        <img alt="img" src={require("./assets/img-2.png")} />
+        <img alt="img" src={require("./assets/img-4.png")} />
+        <img alt="img" src={require("./assets/img-5.png")} />
+        <img alt="img" src={require("./assets/img-6.png")} />
+        <img alt="img" src={require("./assets/img-7.png")} />
+        <img alt="img" src={require("./assets/img-8.png")} />
+        <img alt="img" src={require("./assets/img-9.png")} />
+        <img alt="img" src={require("./assets/img-10.png")} />
+        <img alt="img" src={require("./assets/img-11.png")} />
+        <img className="open" alt="img" src={require("./assets/img-12.png")} />
+        <img  alt="img" src={require("./assets/img-13.png")} />
+        <img  alt="img" src={require("./assets/img-14.png")} />
+        <img alt="img" src={require("./assets/img-15.png")} />
+        <img alt="img" src={require("./assets/img-16.png")} />
+        <img alt="img" src={require("./assets/img-17.png")} />
+        <img alt="img" src={require("./assets/img-18.png")} />
+        <img alt="img" src={require("./assets/img-19.png")} />
+        <img alt="img" src={require("./assets/img-20.png")} />
+        <img alt="img" src={require("./assets/img-21.png")} />
+        <img alt="img" src={require("./assets/img-22.png")} />
+        <img alt="img" src={require("./assets/img-23.png")} />
+        <img className="open" alt="img" src={require("./assets/img-24.png")} />
+        <img alt="img" src={require("./assets/img-25.png")} />
+        <img alt="img" src={require("./assets/img-26.png")} />
+        <img alt="img" src={require("./assets/img-27.png")} />
+        <img alt="img" src={require("./assets/img-28.png")} />
+        <img alt="img" src={require("./assets/img-29.png")} />
+        <img alt="img" src={require("./assets/img-30.png")} />
+        <img alt="img" src={require("./assets/img-31.png")} />
+        <img alt="img" src={require("./assets/img-32.png")} />
+        <img alt="img" src={require("./assets/img-33.png")} />
+        <img alt="img" src={require("./assets/img-34.png")} />
+        <img alt="img" src={require("./assets/img-35.png")} />
+        <img className="open" alt="img" src={require("./assets/img-36.png")} />
+        <img alt="img" src={require("./assets/img-37.png")} />
+        <img alt="img" src={require("./assets/img-38.png")} />              
+      </section>
+     
+      <h3 align="center">Artists:</h3>
+      <p align="center">Alex & Ty Flannagan</p>
+  
+    
+    
+    </wrapper>
+    </>
   );
 }
 
