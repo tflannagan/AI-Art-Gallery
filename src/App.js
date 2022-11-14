128d41c0d4ed6baf323980be5570d05c084487f7
Ty Flannagan <tylerflannagan@yahoo.com>
2022-11-14 13:09:28 -0800

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -45,90 +45,90 @@ function App() {
         <img alt="img" src={require("./assets/img-34.png")} />
         <img alt="img" src={require("./assets/img-35.png")} />
         <img className="open" alt="img" src={require("./assets/img-36.png")} />
-        <img alt="img" src={require("./assets/img-37.png")} />
-        <img alt="img" src={require("./assets/img-38.png")} />    
-        <img alt="img" src={require("./assets/img-4.png")} />
-        <img alt="img" src={require("./assets/img-5.png")} />
-        <img alt="img" src={require("./assets/img-6.png")} />
-        <img alt="img" src={require("./assets/img-14.png")} />
-        <img alt="img" src={require("./assets/img-15.png")} />
-        <img alt="img" src={require("./assets/img-16.png")} />
-        <img alt="img" src={require("./assets/img-21.png")} />
-        <img alt="img" src={require("./assets/img-22.png")} />
-        <img alt="img" src={require("./assets/img-39.jpg")} />  
-        <img className="open" alt="img" src={require("./assets/img-40.jpg")} /> 
-        <img alt="img" src={require("./assets/img-49.png")} />
-        <img alt="img" src={require("./assets/img-50.png")} />    
-        <img alt="img" src={require("./assets/img-51.png")} />
-        <img alt="img" src={require("./assets/img-52.png")} />
-        <img alt="img" src={require("./assets/img-53.png")} />
-        <img alt="img" src={require("./assets/img-54.png")} />
-        <img alt="img" src={require("./assets/img-55.png")} />
-        <img alt="img" src={require("./assets/img-56.png")} />
-        <img alt="img" src={require("./assets/img-57.png")} />
-        <img alt="img" src={require("./assets/img-58.png")} />
-        <img alt="img" src={require("./assets/img-59.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-60.png")} /> 
-        <img alt="img" src={require("./assets/img-61.png")} />
-        <img alt="img" src={require("./assets/img-62.png")} />    
-        <img alt="img" src={require("./assets/img-63.png")} />
-        <img alt="img" src={require("./assets/img-64.png")} />
-        <img alt="img" src={require("./assets/img-65.png")} />
-        <img alt="img" src={require("./assets/img-66.png")} />
-        <img alt="img" src={require("./assets/img-67.png")} />
-        <img alt="img" src={require("./assets/img-68.png")} />
-        <img alt="img" src={require("./assets/img-69.png")} />
-        <img alt="img" src={require("./assets/img-70.png")} />
-        <img alt="img" src={require("./assets/img-71.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-72.png")} /> 
-        <img alt="img" src={require("./assets/img-73.png")} />
-        <img alt="img" src={require("./assets/img-74.png")} />    
-        <img alt="img" src={require("./assets/img-75.png")} />
-        <img alt="img" src={require("./assets/img-76.png")} />
-        <img alt="img" src={require("./assets/img-77.png")} />
-        <img alt="img" src={require("./assets/img-78.png")} />
-        <img alt="img" src={require("./assets/img-79.png")} />
-        <img alt="img" src={require("./assets/img-80.png")} />
-        <img alt="img" src={require("./assets/img-81.png")} />
-        <img alt="img" src={require("./assets/img-82.png")} />
-        <img alt="img" src={require("./assets/img-83.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-84.png")} />    
-        <img alt="img" src={require("./assets/img-85.png")} />
-        <img alt="img" src={require("./assets/img-86.png")} />    
-        <img alt="img" src={require("./assets/img-87.png")} />
-        <img alt="img" src={require("./assets/img-88.png")} />
-        <img alt="img" src={require("./assets/img-89.png")} />
-        <img alt="img" src={require("./assets/img-90.png")} />
-        <img alt="img" src={require("./assets/img-91.png")} />
-        <img alt="img" src={require("./assets/img-92.png")} />
-        <img alt="img" src={require("./assets/img-93.png")} />
-        <img alt="img" src={require("./assets/img-94.png")} />
-        <img alt="img" src={require("./assets/img-95.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-96.png")} /> 
-        <img alt="img" src={require("./assets/img-97.png")} />
-        <img alt="img" src={require("./assets/img-98.png")} />    
-        <img alt="img" src={require("./assets/img-99.png")} />
-        <img alt="img" src={require("./assets/img-100.png")} />
-        <img alt="img" src={require("./assets/img-101.png")} />
-        <img alt="img" src={require("./assets/img-102.png")} />
-        <img alt="img" src={require("./assets/img-103.png")} />
-        <img alt="img" src={require("./assets/img-104.png")} />
-        <img alt="img" src={require("./assets/img-105.png")} />
-        <img alt="img" src={require("./assets/img-106.png")} />
-        <img alt="img" src={require("./assets/img-107.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-108.png")} />
-        <img alt="img" src={require("./assets/img-109.png")} />
-        <img alt="img" src={require("./assets/img-110.png")} />    
-        <img alt="img" src={require("./assets/img-111.png")} />
-        <img alt="img" src={require("./assets/img-112.png")} />
-        <img alt="img" src={require("./assets/img-113.png")} />
-        <img alt="img" src={require("./assets/img-114.png")} />
-        <img alt="img" src={require("./assets/img-115.png")} />
-        <img alt="img" src={require("./assets/img-116.png")} />
-        <img alt="img" src={require("./assets/img-117.png")} />
-        <img alt="img" src={require("./assets/img-118.png")} />
-        <img alt="img" src={require("./assets/img-119.png")} />  
-        <img className="open" alt="img" src={require("./assets/img-120.png")} />              
+        <img alt="img" src={require("./assets/img-37.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-38.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-4.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-5.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-6.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-14.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-15.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-16.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-21.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-22.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-39.jpg")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-40.jpg")} loading="lazy" /> 
+        <img alt="img" src={require("./assets/img-49.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-50.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-51.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-52.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-53.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-54.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-55.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-56.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-57.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-58.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-59.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-60.png")} loading="lazy" /> 
+        <img alt="img" src={require("./assets/img-61.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-62.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-63.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-64.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-65.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-66.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-67.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-68.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-69.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-70.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-71.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-72.png")} loading="lazy" /> 
+        <img alt="img" src={require("./assets/img-73.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-74.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-75.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-76.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-77.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-78.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-79.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-80.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-81.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-82.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-83.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-84.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-85.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-86.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-87.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-88.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-89.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-90.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-91.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-92.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-93.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-94.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-95.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-96.png")} loading="lazy" /> 
+        <img alt="img" src={require("./assets/img-97.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-98.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-99.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-100.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-101.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-102.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-103.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-104.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-105.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-106.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-107.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-108.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-109.png")} loading="lazy"/>
+        <img alt="img" src={require("./assets/img-110.png")} loading="lazy" />    
+        <img alt="img" src={require("./assets/img-111.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-112.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-113.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-114.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-115.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-116.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-117.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-118.png")} loading="lazy" />
+        <img alt="img" src={require("./assets/img-119.png")} loading="lazy" />  
+        <img className="open" alt="img" src={require("./assets/img-120.png")} loading="lazy" />              
       </section>
      
       <h3 align="center">Prompt Engineers:</h3>
