f69889821310c0d45bebae9c5ddc8c41d0d372a6
Ty Flannagan <tylerflannagan@yahoo.com>
2022-12-07 21:14:43 -0800

    updated ui and functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,616 @@
 import './App.css';
 import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import Placeholder from './Placeholder'; //Placeholder component.
+//Import the css file if using blur, black and white, or opacity effects. 
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+
 
 function App() {
- 
-
+    
   return (
     <>
       <h1 align="center">AI Art Gallery</h1>
-     <wrapper>
+    
       <section className="App">
-        <img  alt="img" src={require("./assets/img-1.png")} />
+        <img className="image" alt="img" src={require("./images/img-1.png")} />
+        <img className="image" alt="img" src={require("./images/img-3.png")} />
+        <img className="image" alt="img" src={require("./images/img-2.png")} />
+        <img className="image" alt="img" src={require("./images/img-41.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-42.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-43.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-7.png")} />
+        <img className="image" alt="img" src={require("./images/img-8.png")} />
+        <img className="image" alt="img" src={require("./images/img-9.png")} />
+        <img className="image" alt="img" src={require("./images/img-10.png")} />
+        <img className="image" alt="img" src={require("./images/img-11.png")} />
+        <img className="image" alt="img" src={require("./images/img-12.png")} />
+        <img className="image" alt="img" src={require("./images/img-13.png")} />
+        <img className="image" alt="img" src={require("./images/img-45.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-44.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-46.jpg")} />
+        <img className="image" alt="img" src={require("./images/img-17.png")} />
+        <img className="image" alt="img" src={require("./images/img-18.png")} />
+
+
+       {/* //Lazy Loaded Images */}
+        
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-19.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-20.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-47.jpg")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-48.jpg")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-24.png")}
+        />  
+
+       
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-25.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-23.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-26.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-27.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-28.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-29.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-30.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-31.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-32.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-33.png")}
+        />  
+
+      <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-34.png")}
+        />      
+     
+      <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-35.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-36.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-37.png")}
+        />  
+
+
+      <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-38.png")}
+        />  
+
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-39.jpg")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-40.jpg")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-49.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-50.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-51.png")}
+        />  
+
+       
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-52.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-53.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-54.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-55.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-56.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-57.png")}
+        />      
+     
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-58.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-59.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-60.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-61.png")}
+        />  
+
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-62.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-63.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-64.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-65.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-66.png")}
+        />  
+         <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-67.png")}
+        />  
+
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-68.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-69.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-70.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-71.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-72.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-73.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-74.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-75.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-76.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-77.png")}
+        />  
+
+      <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-78.png")}
+        />      
+     
+      <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-79.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-80.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-81.png")}
+        />  
+
+
+      <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-82.png")}
+        />  
+
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-83.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-84.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-85.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-86.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-87.png")}
+        />  
+
+       
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-88.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-89.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-90.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-91.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-92.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-93.png")}
+        />      
+     
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-94.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-95.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-96.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-97.png")}
+        />  
+
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-98.png")}
+        />      
+     
+       <LazyLoadImage
+       className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-99.png")}
+        />  
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-100.png")}
+        />  
+
+
+      <LazyLoadImage
+      className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-101.png")}
+        />  
+
+
+        <LazyLoadImage
+        className="image"
+              alt="img"
+              effect="blur"
+              src={require("./images/img-102.png")}
+        />  
+
+
+
+     
+        {/* <img  alt="img" src={require("./assets/img-1.png")} />
         <img alt="img" src={require("./assets/img-3.png")} />
         <img alt="img" src={require("./assets/img-2.png")} />
         <img alt="img" src={require("./assets/img-41.jpg")} />
@@ -27,6 +628,9 @@ function App() {
         <img alt="img" src={require("./assets/img-46.jpg")} />
         <img alt="img" src={require("./assets/img-17.png")} />
         <img alt="img" src={require("./assets/img-18.png")} />
+
+
+
         <img alt="img" src={require("./assets/img-19.png")} />
         <img alt="img" src={require("./assets/img-20.png")} />
         <img alt="img" src={require("./assets/img-47.jpg")} />
@@ -128,7 +732,7 @@ function App() {
         <img alt="img" src={require("./assets/img-117.png")} loading="lazy" />
         <img alt="img" src={require("./assets/img-118.png")} loading="lazy" />
         <img alt="img" src={require("./assets/img-119.png")} loading="lazy" />  
-        <img className="open" alt="img" src={require("./assets/img-120.png")} loading="lazy" />              
+        <img className="open" alt="img" src={require("./assets/img-120.png")} loading="lazy" />               */}
       </section>
      
       <h3 align="center">Prompt Engineers:</h3>
@@ -136,7 +740,7 @@ function App() {
   
     
     
-    </wrapper>
+  
     </>
   );
 }
