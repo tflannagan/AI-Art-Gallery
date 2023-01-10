9fe18b89e5d5d4e9ff492edd3543e5d000cf0b1f
Ty Flannagan <tylerflannagan@yahoo.com>
2023-01-09 20:23:55 -0800

    full overhaul

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    
+    return(
+        <div className="header-container">
+          <h1 align="center">AI Art Gallery</h1>
+        </div>
+    );
+}
+
+export default Header;
