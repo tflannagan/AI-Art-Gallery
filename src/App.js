24f05e02cd7388278d570eb97e86c8838a068c86
Ty Flannagan <tylerflannagan@yahoo.com>
2023-04-03 22:35:07 -0700

    added Footer component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import img1 from "./images/img-1.png";
 import img2 from "./images/img-23.png";
 import img3 from "./images/img-14.png";
+import img4 from "./images/img-32.png";
 
 //Components
 import Header from "./components/Header";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,10 +18,12 @@ function App() {
         <Card imgSrc={img1} />
         <Card imgSrc={img2} />
         <Card imgSrc={img3} />
+        <Card imgSrc={img4} />
       </section>
-
+      <Footer />
+      {/* 
       <h3 align="center">Prompt Engineers:</h3>
-      <p align="center">Alex & Ty Flannagan</p>
+      <p align="center">Alex & Ty Flannagan</p> */}
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <h3 align="center">Prompt Engineers:</h3>
+      <p align="center">Alex & Ty Flannagan</p>
+    </div>
+  );
+}
+
+export default Footer;
